3bb13150e5e51cca73ee278310c32a795a32f4d8
Aidan Sawyer <aks5238@rit.edu>
2016-02-08 16:46:02 +0000

    begin work on mediator implementation

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+  <title>02-DesignPatterns</title>
+</head>
+
+  <body>
+  
+    <p> Common Web Design Patterns </p>
+    <ul>
+      <li> <a href="./Adapter/adapter.html" > Adapter </a> </li>
+      <li> <a href="./Facade/facade.html" > Facade </a> </li>
+      <li> <a href="./InversionOfControl/inversionOfControl.html" > Inversion of Control </a> </li>
+      <li> <a href="./Mediator/mediator.html" > Mediator </a> </li>
+    </ul>
+  
+  </body>
+
+</html>
